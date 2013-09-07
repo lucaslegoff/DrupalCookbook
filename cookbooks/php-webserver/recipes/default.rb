7565b27dbf9b8fdc9a4c1096d60ee36ca2531bc8
Lucas Le Goff <lucas.legoff@gmail.com>
2013-09-07 18:10:25 +0200

    Change from using a role to using a cookbook

---
+++ cookbooks/php-webserver/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: php-webserver
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "apt"
+include_recipe "servertools"
+include_recipe "php-webserver::apache"
