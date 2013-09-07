898babf8301fc9cb3ed1f8ae08d20e3b2b11803b
Lucas Le Goff <lucas.legoff@gmail.com>
2013-09-07 09:58:29 +0200

    apache config

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,2 @@
+default['apache']['listen_ports'] = ["80"]
+default['apache']['contact'] = "ops@example.com"
--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -6,3 +6,18 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package "apache2" do
+	action :install
+end
+
+service "apache2" do	
+    supports [:restart, :reload]
+  	action :enable
+end
+
+template "/etc/apache2/ports.conf" do
+  source "ports.conf.erb"
+  notifies :restart, "service[apache2]"
+end
+
