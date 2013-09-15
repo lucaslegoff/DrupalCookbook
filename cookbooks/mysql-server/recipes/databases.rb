14503006c86f85779a8e4941c933ebaf76416852
Lucas Le Goff <lucas.legoff@gmail.com>
2013-09-15 23:07:12 +0200

    added Database user creation and grant

--- cookbooks/mysql-server/recipes/databases.rb
+++ cookbooks/mysql-server/recipes/databases.rb
@@ -12,5 +12,20 @@ if node["databases"]
 	      connection mysql_connection_info
 	      action :create
 	    end
+
+	    mysql_database_user databaseProp["account_name"] do
+		  connection mysql_connection_info
+		  password databaseProp["account_pass"]
+		  database_name key
+		  host '%'
+		  action :create
+		end
+
+		mysql_database_user databaseProp["account_name"] do
+		  connection mysql_connection_info
+		  database_name key
+		  privileges [:all]
+		  action :grant
+		end
 	end
 end
