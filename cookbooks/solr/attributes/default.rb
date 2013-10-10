7d7640f7019dbdacd15dda0782b69e0d1044f6af
Lucas Le Goff <lucas.legoff@gmail.com>
2013-10-10 23:37:04 +0200

    Creation of solr cookbook : tomcat setup

---
+++ cookbooks/solr/attributes/default.rb
@@ -0,0 +1,3 @@
+override["tomcat"]["port"] = 8983
+override['tomcat']['keytool'] = "/usr/lib/jvm/java-6-openjdk/bin/keytool"
+override['java']['java_home'] = "/usr/lib/jvm/java-6-openjdk"
--- solo.rb
+++ solo.rb
@@ -1,2 +1,3 @@
 file_cache_path "/var/chef/cache"
 cookbook_path [ "/var/chef/DrupalChef/cookbooks", "/var/chef/DrupalChef/site-cookbooks" ]
+data_bag_path [ "/var/chef/DrupalChef/data_bags" ]
