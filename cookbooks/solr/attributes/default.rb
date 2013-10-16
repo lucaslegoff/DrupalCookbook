0268ff361b3bf0245a4b857a6ba73589a3ffa089
Lucas Le Goff <lucas.legoff@gmail.com>
2013-10-16 22:14:35 +0200

    solr cookbook : download and extract

--- cookbooks/solr/attributes/default.rb
+++ cookbooks/solr/attributes/default.rb
@@ -1,3 +1,20 @@
 override["tomcat"]["port"] = 8983
 override['tomcat']['keytool'] = "/usr/lib/jvm/java-6-openjdk/bin/keytool"
 override['java']['java_home'] = "/usr/lib/jvm/java-6-openjdk"
+
+
+default['solr']['version'] = "3.6.1"
+default['solr']['checksum'] = "1b4552ba95c8456d4fbd596e82028eaa0619b6942786e98e1c4c31258543c708" #sha265
+default['solr']['directory'] = "/usr/local/src"
+
+if solr.version.split('.').first.to_i >= 4
+  default['solr']['link']      = "http://archive.apache.org/dist/lucene/solr/#{default['solr']['version']}/solr-#{default['solr']['version']}.tgz"
+  default['solr']['download']  = "#{default['solr']['directory']}/solr-#{default['solr']['version']}.tgz"
+  default['solr']['extracted'] = "#{default['solr']['directory']}/solr-#{default['solr']['version']}"
+  default['solr']['war']       = "#{default['solr']['extracted']}/dist/solr-#{default['solr']['version']}.war"
+else
+  default['solr']['link']      = "http://archive.apache.org/dist/lucene/solr/#{default['solr']['version']}/apache-solr-#{default['solr']['version']}.tgz"
+  default['solr']['download']  = "#{default['solr']['directory']}/apache-solr-#{default['solr']['version']}.tgz"
+  default['solr']['extracted'] = "#{default['solr']['directory']}/apache-solr-#{default['solr']['version']}"
+  default['solr']['war']       = "#{default['solr']['extracted']}/dist/apache-solr-#{default['solr']['version']}.war"
+end
--- cookbooks/solr/recipes/default.rb
+++ cookbooks/solr/recipes/default.rb
@@ -9,3 +9,16 @@
 
 include_recipe "tomcat"
 include_recipe "tomcat::users"
+
+#Download solr archive
+remote_file node['solr']['download'] do
+  source   node['solr']['link']
+  checksum node['solr']['checksum']
+  mode     0644
+end
+
+#extract content
+bash 'unpack solr' do
+  code   "tar xzf #{node['solr']['download']} -C #{node['solr']['directory']}"
+  not_if "test -d #{node['solr']['extracted']}"
+end
