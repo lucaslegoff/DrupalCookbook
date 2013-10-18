2ace67ef6dd2097fe89adbb05118a5fad8a2feee
Lucas Le Goff <lucas.legoff@gmail.com>
2013-10-18 22:35:45 +0200

    Copy solr config files and war file

--- cookbooks/solr/attributes/default.rb
+++ cookbooks/solr/attributes/default.rb
@@ -2,7 +2,6 @@ override["tomcat"]["port"] = 8983
 override['tomcat']['keytool'] = "/usr/lib/jvm/java-6-openjdk/bin/keytool"
 override['java']['java_home'] = "/usr/lib/jvm/java-6-openjdk"
 
-
 default['solr']['version'] = "3.6.1"
 default['solr']['checksum'] = "1b4552ba95c8456d4fbd596e82028eaa0619b6942786e98e1c4c31258543c708" #sha265
 default['solr']['directory'] = "/usr/local/src"
@@ -18,3 +17,6 @@ else
   default['solr']['extracted'] = "#{default['solr']['directory']}/apache-solr-#{default['solr']['version']}"
   default['solr']['war']       = "#{default['solr']['extracted']}/dist/apache-solr-#{default['solr']['version']}.war"
 end
+
+default['solr']['instanceName'] = "defaultSolr"
+default['solr']['home'] = "/opt/solr/#{default['solr']['instanceName']}"
--- cookbooks/solr/recipes/default.rb
+++ cookbooks/solr/recipes/default.rb
@@ -22,3 +22,22 @@ bash 'unpack solr' do
   code   "tar xzf #{node['solr']['download']} -C #{node['solr']['directory']}"
   not_if "test -d #{node['solr']['extracted']}"
 end
+
+directory node['solr']['home'] do
+  owner node["tomcat"]["user"]
+  group node['tomcat']['group']
+  mode "755"
+  recursive true
+  action :create
+end
+
+#copy files to solr home
+bash "copy config files" do
+  code "cp -r #{node.solr.extracted}/example/solr/* #{node.solr.home}"
+end
+
+bash "copy war files" do
+  code "cp #{node.solr.war} #{node.solr.home}/"
+  creates #{node.solr.war}
+end
+
